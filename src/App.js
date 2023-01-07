3ac31de39dce59a397758016680b3cfd658d364a
ameya-6964 <belvalkar7@gmail.com>
2023-01-07 17:20:01 +0530

    Completed The Webpack Tutorial

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+const App = () => {
+  return(
+    <div>
+    
+    <h1>
+        Welcome To Talent Battle React Webpack Session
+    </h1>
+    <h2>
+    After all — necessity is the mother of all invention…
+    </h2>
+
+    </div>
+  );
+};
+
+export default App;
